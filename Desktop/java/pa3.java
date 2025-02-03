916b7693ba85aa67f6c3667cc938f966a7ba1333
mk <machindrakharat6665@gmail.com>
2025-02-03 14:06:14 +0530

    pa3.java

---
+++ Desktop/java/pa3.java
@@ -0,0 +1,18 @@
+public class pa3 {
+    public static void main(String args[]){
+        int i,j;
+        for(i=0;i<=5;i++){
+            for(j=0;j<=i;j++){
+                if(i==1||i==4||j==0||j==4)
+                    System.out.print("*");
+                    else{
+                        System.out.println("");
+                    }
+                }
+                System.out.print("*");
+            }
+            
+        }
+    }
+    
+
